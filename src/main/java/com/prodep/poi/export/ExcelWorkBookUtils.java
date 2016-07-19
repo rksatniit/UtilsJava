a34b50ce7896d460019fa774f7a57619482088f3
rguntuka <RAMAKRISHNA.GUNTUKA@adtran.com>
2016-07-19 18:42:23 +0530

    Changes to avoid methods

--- src/main/java/com/prodep/poi/export/ExcelWorkBookUtils.java
+++ src/main/java/com/prodep/poi/export/ExcelWorkBookUtils.java
@@ -20,11 +20,6 @@ import com.prodep.poi.mongodb.EmployeeDataBroker;
 public class ExcelWorkBookUtils {
 
 	public static void createWorkBook(String withFileName) throws Exception{
-<<<<<<< HEAD
-		//Create Blank workbook
-		//emptyWorkBook(withFileName);
-=======
->>>>>>> 37dd338... RestWithPOI Changes
 
 		workBookWithData(withFileName);
 	}
@@ -129,14 +124,5 @@ public class ExcelWorkBookUtils {
 		System.out.println(withFileName + " written successfully");
 	}
 
-<<<<<<< HEAD
-	public static void readFromDB() throws Exception {
 
-
-	}
-	public static void createExcelFromDB() throws Exception {
-
-	}
-=======
->>>>>>> 37dd338... RestWithPOI Changes
 }
